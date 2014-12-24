32bc1b9e9c48ba2a7c6f9de0d939460846e2c185
Larkin Whitaker <larkin@elkdevelopment.com>
2014-12-24 07:24:35 +0000

    asdfasdfdsasdfadsf

--- app/views/slideshow.blade.php
+++ app/views/slideshow.blade.php
@@ -59,7 +59,7 @@
                     </div>
                 </div>
 
-                    <audio controls style="width:100%;" preload="auto" oncanplaythrough="this.play();">
+                    <audio controls style="width:100%;" preload="auto" oncanplaythrough="this.play();" loop>
                         <source src="{{ asset('assets/audio/beatles.mp3') }}" type="audio/mpeg">
                         Your browser does not support the audio element.
                     </audio>
@@ -137,7 +137,7 @@
 
             var options = {
                 $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-                $AutoPlayInterval: 1500,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+                $AutoPlayInterval: 5000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
                 $PauseOnHover: 1,                                //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
 
                 $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
